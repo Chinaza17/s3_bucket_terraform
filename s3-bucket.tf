decf80e52e68e87eda4cc18854881a82168338ac
Ubuntu <ubuntu@ip-172-31-47-81.eu-west-2.compute.internal>
2024-11-14 04:32:57 +0000

    Add Terraform file for S3 bucket

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-2"  # Change to your desired region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-bucket"  # Ensure this is a globally unique name
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  acl    = "private"  # Set the ACL to private
+}
