10121c6d9e0b0b5f6fa9fb51b7f148ad91a7662d
Ubuntu <ubuntu@ip-172-31-47-81.eu-west-2.compute.internal>
2024-11-14 05:55:59 +0000

    Add unique suffix to S3 bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,13 @@
-provider "aws" {
-  region = "eu-west-2"  # Change to your desired region
+resource "random_string" "unique_suffix" {
+  length  = 8
+  special = false
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-terraform-bucket"  # Ensure this is a globally unique name
+  bucket = "my-terraform-buckett-${random_string.unique_suffix.result}"
 }
 
 resource "aws_s3_bucket_acl" "my_bucket_acl" {
   bucket = aws_s3_bucket.my_bucket.bucket
-  acl    = "private"  # Set the ACL to private
+  acl    = "private"
 }
